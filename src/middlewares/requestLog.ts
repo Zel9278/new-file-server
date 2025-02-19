a27bb032d095285ef2e4e2d2ca24dbd2f0b85322
Zel9278 <countbaster@gmail.com>
2025-02-19 12:28:07 +0000

    545435435

--- src/middlewares/requestLog.ts
+++ src/middlewares/requestLog.ts
@@ -1,25 +1,37 @@
 import isBrowser from "@/utils/checkBrowserRegex"
 import { Middleware } from "./middlewareChain"
+import chalk from "chalk"
+
+chalk.level = 3
 
 export const requestLogMiddleware: Middleware = async (req, _event, next) => {
   const userAgent = req.headers.get("user-agent")
 
   if (isBrowser(userAgent)) {
-    console.log("This is a browser request")
+    console.log(chalk.blue("This is a browser request"))
   } else {
-    console.log("This is not a browser request")
+    console.log(chalk.yellow("This is not a browser request"))
   }
 
   console.log(
-    `[${new Date().toISOString()}] [Request ]     ${req.method} ${req.url} | ${userAgent}`,
+    chalk.gray(`[${new Date().toISOString()}]`) +
+      chalk.cyan(` [Request ]     `) +
+      chalk.green(`${req.method} `) +
+      chalk.white(`${req.url}`) +
+      chalk.gray(` | ${userAgent}`),
   )
 
   const response = await next()
 
   console.log(
-    `[${new Date().toISOString()}] [Response] ${
-      response.status
-    } ${req.method} ${req.url} | ${userAgent}`,
+    chalk.gray(`[${new Date().toISOString()}]`) +
+      chalk.magenta(` [Response] `) +
+      chalk.hex(response.status === 200 ? "#00FF00" : "#FF0000")(
+        `${response.status} `,
+      ) +
+      chalk.green(`${req.method} `) +
+      chalk.white(`${req.url}`) +
+      chalk.gray(` | ${userAgent}`),
   )
 
   return response
